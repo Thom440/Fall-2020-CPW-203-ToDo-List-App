84423830e9350a829e7a68701f3ceff79b518a24
Thom Barrett <thom440@yahoo.com>
2020-11-02 12:47:04 -0800

    Created main.ts and main.js

---
+++ ts/main.ts
@@ -0,0 +1,29 @@
+class ToDoItem {
+    title:string;
+    dueDate:Date;
+    isCompleted:boolean;
+}
+
+/**
+ * Check form data is valid
+ */
+function isValid():boolean {
+
+}
+
+/**
+ * Get all input off form and wrap in a ToDoItem object
+ */
+function getToDoItem():ToDoItem {
+
+}
+
+/**
+ * Display ToDoItem on webpage
+ */
+function displayToDoItem(item:ToDoItem):void {
+
+}
+
+// Task: Allow user to mark item complete
+// Store items in web storage
