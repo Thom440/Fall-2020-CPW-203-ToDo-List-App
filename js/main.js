8a6619c01d6901c03ddc13096fc6d7a8fdd6c58c
Thom Barrett <thom440@yahoo.com>
2020-11-02 13:44:29 -0800

    Created ToDo item and display ToDo Item

--- js/main.js
+++ js/main.js
@@ -3,9 +3,39 @@ var ToDoItem = (function () {
     }
     return ToDoItem;
 }());
+window.onload = function () {
+    var addItem = document.getElementById("add-item");
+    addItem.onclick = main;
+};
+function main() {
+    if (isValid()) {
+        var item = getToDoItem();
+        displayToDoItem(item);
+    }
+}
 function isValid() {
+    return true;
 }
 function getToDoItem() {
+    var item = new ToDoItem;
+    item.title = getById("title").value;
+    item.dueDate = new Date(getById("due-date").value);
+    item.isCompleted = getById("complete").checked;
+    return item;
 }
 function displayToDoItem(item) {
+    var text = document.createElement("h3");
+    text.innerText = item.title;
+    var date = document.createElement("p");
+    date.innerText = item.dueDate.toDateString();
+    var itemDiv = document.createElement("div");
+    if (item.isCompleted) {
+        itemDiv.classList.add("completed");
+    }
+    itemDiv.appendChild(text);
+    itemDiv.appendChild(date);
+    document.getElementById("incomplete").appendChild(itemDiv);
+}
+function getById(id) {
+    return document.getElementById(id);
 }
--- ts/main.ts
+++ ts/main.ts
@@ -4,25 +4,60 @@ class ToDoItem {
     isCompleted:boolean;
 }
 
+window.onload = function() {
+    let addItem = document.getElementById("add-item");
+    addItem.onclick = main;
+}
+
+function main() {
+    if (isValid()) {
+        let item = getToDoItem();
+        displayToDoItem(item);
+    }
+}
+
 /**
  * Check form data is valid
  */
 function isValid():boolean {
-
+    return true;
 }
 
 /**
  * Get all input off form and wrap in a ToDoItem object
  */
 function getToDoItem():ToDoItem {
-
+    let item = new ToDoItem;
+    item.title = getById("title").value;
+    item.dueDate = new Date(getById("due-date").value);
+    item.isCompleted = getById("complete").checked;
+    return item;
 }
 
 /**
  * Display ToDoItem on webpage
  */
 function displayToDoItem(item:ToDoItem):void {
+    let text = document.createElement("h3");
+    text.innerText = item.title;
 
+    let date = document.createElement("p");
+    date.innerText = item.dueDate.toDateString();
+
+    let itemDiv = document.createElement("div");
+    if (item.isCompleted) {
+        itemDiv.classList.add("completed");
+    }
+
+    itemDiv.appendChild(text);
+    itemDiv.appendChild(date);
+
+    document.getElementById("incomplete").appendChild(itemDiv);
+
+}
+
+function getById(id):HTMLInputElement {
+    return <HTMLInputElement>document.getElementById(id);
 }
 
 // Task: Allow user to mark item complete
